4ccf4af034742682f5c47c83d446ab2ab6ef4c04
Ogulcan <ogulcankirtay8@gmail.com>
2025-11-10 19:59:58 +0300

    initial commit project started

---
+++ src/main/java/com/oglcnkrty/starter/JwtApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.oglcnkrty.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApplicationStarter.class, args);
+    }
+
+}
---
+++ src/test/java/com/oglcnkrty/starter/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oglcnkrty.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
